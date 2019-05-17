5f2becb8422ae5ee9d07e0899ef009da13b6d6bd
SharanyaSudhakar <32864742+SharanyaSudhakar@users.noreply.github.com>
2019-05-17 00:06:42 -0700

    newupdate
    flipper test scenes added

---
+++ state_unity/Assets/Scripts/Fish_Gameobject_Behaviour.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class Fish_Gameobject_Behaviour : MonoBehaviour {
+
+
+    readonly int dropfish = Animator.StringToHash("dropFish");
+    Animator aniamtor;
+    bool movedown = false, ismoveforward = false;
+    public Transform pos, pos2; //pos2 ued by salmon 2&3
+    Quaternion rot;
+
+    // Use this for initialization
+    void Start () {
+        aniamtor = GetComponent<Animator>();
+        rot = new Quaternion(0,1.2f, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (movedown)
+        {
+            transform.position = Vector3.Lerp(transform.position, pos.position, Time.deltaTime * 0.4f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rot, 0.4f * Time.deltaTime);
+            if (Vector3.Distance(transform.position, pos.position) < .04f)
+                movedown = false;
+        }
+        if (ismoveforward)
+        {
+            transform.position = Vector3.Lerp(transform.position, pos2.position, Time.deltaTime*1.4f);
+            if (Vector3.Distance(transform.position,pos2.position) < .04f)
+                ismoveforward = false;
+        }
+            
+	}
+    public void show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void unbend()
+    {
+        gameObject.GetComponent<ParentConstraint>().constraintActive = false;
+        aniamtor.SetTrigger(dropfish);
+        movedown = true;
+    }
+
+    public void moveforward()
+    {
+        ismoveforward = true;
+    }
+
+    //public void OnCollisionEnter(Collision collision)
+    //{
+    //    GetComponent<Rigidbody>().AddForce(collision.transform.forward,ForceMode.Force);
+    //}
+}
---
+++ state_unity/Assets/Scripts/GrabScript.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabScript : MonoBehaviour
+{
+
+    private bool rightHandtouched = false;
+    private bool leftHandtouched = false;
+    public GameObject rightHandDisable, granny;
+    public GameObject leftHandDisable;
+    private Collider rightHand;
+    private Collider leftHand;
+    GameObject fish;
+
+    public Transform leftholder, rightholder;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void celebrate()
+    {
+        granny.GetComponent<Granny_Behavior>().chewing.Play();
+        granny.GetComponent<Granny_Behavior>().EatenAndCelebration();
+        if (gameObject.CompareTag("Salmon3"))
+            granny.GetComponent<Granny_Behavior>().Clap3();
+        else if (gameObject.CompareTag("Fish"))
+            granny.GetComponent<Granny_Behavior>().Clap2();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            celebrate();
+        }
+    }
+        //if (other.gameObject.CompareTag("PlayerLeftHand"))
+        //{
+        //    Debug.Log("Left Hand touched");
+        //    leftHandtouched = true;
+        //    rightHandtouched = false;
+        //    leftHand = other;
+        //    rightHandDisable.tag = "Player";
+        //}
+        //else if (other.gameObject.CompareTag("PlayerRightHand"))
+        //{
+        //    Debug.Log("Right Hand touched");
+        //    rightHandtouched = true;
+        //    leftHandtouched = false;
+        //    rightHand = other;
+        //    leftHandDisable.tag = "Player";
+        //}
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("PlayerRightHand"))
+    //    {
+    //        rightHandtouched = false;
+    //        leftHandDisable.tag = "PlayerLeftHand";
+    //    }
+    //    else if (other.gameObject.CompareTag("PlayerLeftHand"))
+    //    {
+    //        leftHandtouched = false;
+    //        rightHandDisable.tag = "PlayerRightHand";
+    //    }
+    //}
+
+    //private void stickOnToFlipper(Collider whichhand)
+    //{
+    //    if (whichhand.name == "left_flipper")
+    //        this.transform.position = leftholder.position;
+    //    else
+    //        this.transform.position = rightholder.position;
+    //}
+}
